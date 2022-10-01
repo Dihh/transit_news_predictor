bf9ba9876a2eb1d3213307987f1384d9a33ff88c
Diegton Rodrigues <diegton@gmail.com>
2022-10-01 14:55:35 -0300

    remove text after 2 secs

--- main.js
+++ main.js
@@ -3,4 +3,7 @@ async function getPredict() {
     const reseponse = await fetch(`https://transitaccidentpredictor-jinc6jghnq-uc.a.run.app/predict/${text}`)
     temp = await reseponse.text()
     document.querySelector("#predict").innerHTML = temp
+    setTimeout(() => {
+        document.querySelector("#predict").innerHTML = ""
+    }, 2000)
 }
