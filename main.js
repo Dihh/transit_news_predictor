1f31dbff47a32c320532530e055e64ad0a8ead72
Diegton Rodrigues <diegton@gmail.com>
2022-10-01 14:48:05 -0300

    init

---
+++ main.js
@@ -0,0 +1,6 @@
+async function getPredict() {
+    text = document.querySelector("#formGroupExampleInput").value
+    const reseponse = await fetch(`https://transitaccidentpredictor-jinc6jghnq-uc.a.run.app/predict/${text}`)
+    temp = await reseponse.text()
+    document.querySelector("#predict").innerHTML = temp
+}
